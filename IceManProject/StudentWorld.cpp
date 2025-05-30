5f1853a3f3822749185ae574628da761cbfb21cd
gigi333-d <giestruquel@gmail.com>
2025-05-30 01:56:53 -0700

    Update StudentWorld.cpp

--- IceManProject/StudentWorld.cpp
+++ IceManProject/StudentWorld.cpp
@@ -6,3 +6,17 @@ GameWorld* createStudentWorld(string assetDir)
 {
 	return new StudentWorld(assetDir);
 }
+
+int StudentWorld::move() {
+	player->doSomething();
+	return GWSTATUS_CONTINUE_GAME;
+}
+
+void StudentWorld::cleanUp() {
+	auto i = ice.begin();
+	while (i != ice.end()) {
+		delete(*i);
+		i = ice.erase(i);
+	}
+	delete(player);
+}
