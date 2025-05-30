48ca40f10936f6f6ca49caeb421a3b727ea45353
gigi333-d <giestruquel@gmail.com>
2025-05-30 10:23:25 -0700

    Update StudentWorld.cpp

--- IceManProject/StudentWorld.cpp
+++ IceManProject/StudentWorld.cpp
@@ -7,6 +7,21 @@ GameWorld* createStudentWorld(string assetDir)
 	return new StudentWorld(assetDir);
 }
 
+int StudentWorld::init() {
+    m_player = new Iceman(this);
+
+    for (int i = 0; x < VIEW_WIDTH; ++x) {
+        for (int j = 0; y < VIEW_HEIGHT; ++y) {
+            if (j > 3) {
+		if (i >= 30 && i <= 33) {
+		continue;
+		}
+	    }	
+	Ice* iceField = new Ice(i, j);
+	ice.push_back(iceField);
+        }
+    }
+
 int StudentWorld::move() {
 	player->doSomething();
 	return GWSTATUS_CONTINUE_GAME;
