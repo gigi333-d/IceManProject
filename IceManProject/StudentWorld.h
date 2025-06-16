69f479b9524f6cbb58efa5f98469264745e86376
gigi333-d <giestruquel@gmail.com>
2025-06-16 01:26:28 -0700

    Update StudentWorld.h

--- IceManProject/StudentWorld.h
+++ IceManProject/StudentWorld.h
@@ -7,26 +7,50 @@
 #include <vector>
 #include "Actor.h"
 
+using namespace std;
+
 class Ice;
 class Iceman;
 
 class StudentWorld : public GameWorld
 {
 public:
-    explicit StudentWorld(std::string assetDir);
+    StudentWorld::StudentWorld(std::string assetDir)
+        : GameWorld(assetDir), m_iceman(nullptr) {}
+
     virtual ~StudentWorld();
 
-    // framework required overrides
     int  init() override;
     int  move() override;
     void cleanUp() override;
+    
+	int protestorCount() { return protestorsCount; }
 
-    // helper for Iceman
+    void decProtestors() {protestorsCount--;}
+
+	vector<Ice*> getIce() { return m_ice; }
+	vector<ActivatingObject*> getActivatingObjects() { return ActivatingObjects; }
+	vector<Protestor*> getProtestors() { return Protestors; }
+	vector<Squirt*> getSquirts() { return Squirts; }
+
+	void addActivatingObject(ActivatingObject* a) { ActivatingObjects.push_back(a); }
+
+	double distanceCheck(double aX, double aY, double bX, double bY);
+
+    bool iceCheck(int x, int y);
+
+	void addSquirt(Squirt* s) { Squirts.push_back(s); }
+
     void digIceAt(int x, int y);
 
 private:
-    Iceman* m_iceman = nullptr;
-    std::vector<Ice*> m_ice;
+    int protestorsCount = 0;
+    int pTicks = 200;
+    vector<Ice*> m_ice;
+    vector<ActivatingObject*> ActivatingObjects;
+    vector<Protestor*> Protestors;
+    vector<Squirt*> Squirts;
+	Iceman* m_iceman = nullptr;
 };
 
 #endif 
